4798e964b24e47770ff77848a4dc56be2d7550fa
AgneseLeite <67242970+AgneseLeite@users.noreply.github.com>
2023-01-01 23:53:16 +0000

    Add files via upload

---
+++ scripts.ts
@@ -0,0 +1,17 @@
+/*
+document.addEventListener('DOMContentLoaded', () => {
+    const themeStylesheet = document.getElementById('theme')
+    const themeToggle = document.getElementById('theme-toggle')
+    themeToggle.addEventListener('click', () => {
+      // if it's light -> go dark
+      if (themeStylesheet.href.includes('styles.css')) {
+        themeStylesheet.href = 'styles_dark.css'
+        themeToggle.innerText = 'Back to the light...'
+      } else {
+        // if it's dark -> go light
+        themeStylesheet.href = 'styles.css'
+        themeToggle.innerText = 'To the dark side...'
+      }
+    })
+  })
+*/
